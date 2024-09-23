e6dbf7aeb69a91de1ea68c3b1715a7d0e717865b
Vandana Yadav <vandu565@gmail.com>
2024-09-23 13:55:28 -0400

    Implement the homepage and complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,40 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 export const Home = () => {
-  return <div>Home</div>;
+  const loggedIn={firstName: "Vanadana", lastName:'Yadav', email:'contact@jsmastery.pro'};
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+
+        Recent transactions
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance:123.50},{currentBalance:544.50}]}
+        />
+
+    </section>
+  );
+
 };
 
 export default Home;
---
+++ components/HeaderBox.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from "react";
+
+const headerBox = ({
+  type = "title",
+  title,
+  subtext,
+  user,
+}: HeaderBoxProps) => {
+  return (
+    <div className="header-box">
+        <h1 className="header-box-title">
+             {title}
+             {type=='greeting' && (<span className="text-bankGradient">&nbsp;{user}</span>)}
+        </h1>
+
+        <p className="header-box-subtext"> {subtext}</p>
+
+    </div>
+  );
+};
+
+export default headerBox;
